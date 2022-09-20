d0580a81a3ed3b2af0d358f90c8aad5d873dd76c
Michel Desir <michel.desir79@yahoo.com>
2022-09-20 14:31:59 +0200

    5 commit

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,13 +1,36 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class Launcher {
+    public static long fibo(int nombre)
+    {
+        if(nombre ==0)
+        {
+            return 0;
+        } else if (nombre==1) {
+            return 1;
+        }
+        else {
+            return fibo(nombre-1) + fibo(nombre-2);
+        }
+    }
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvenu: ");
         String mot = scanner.nextLine();
         while(!mot.equals("quit"))
         {
-            System.out.println("Unknown command");
+            if (mot.equals("fibo"))
+            {
+                System.out.println("Choissez un nombre n");
+                String nombre = scanner.nextLine();
+                int inombre = parseInt(nombre);
+                System.out.println(fibo(inombre));
+            }
+            else {
+                System.out.println("Unknown command");
+            }
             System.out.println("Bienvenu: ");
             mot = scanner.nextLine();
         }
